4e4fae77b25e25c0aac017e88e63cc9339366e86
Matthew Belas <belasmatthewkim@gmail.com>
2024-07-01 21:59:13 +0800

    Added animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   onClick(value: string): void {
     if (value === 'x') {
       // window.open('https://x.com/GBILLY_SOL');
@@ -16,5 +16,27 @@ export class AppComponent {
 
   onBuy(): void {
     // window.open('https://pump.fun/BFM9ZghaAWZ4bsQyhCECuSKtNLe7iyjLGTm8SK3upump');
+  }
+
+  textContent: string = 'BELLI TALKSSSS';
+  private hoverInterval: any;
+  private baseText: string = 'BELLI TALKSSS';
+  private hoverText: string = this.baseText;
+
+  onMouseOver(): void {
+    this.hoverInterval = setInterval(() => {
+      this.hoverText += 'S';
+      this.textContent = this.hoverText;
+    }, 500);
+  }
+
+  onMouseOut(): void {
+    clearInterval(this.hoverInterval);
+    this.hoverText = this.baseText;
+    this.textContent = this.hoverText;
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.hoverInterval);
   }
 }
